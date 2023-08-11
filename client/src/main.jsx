dcb1cd11741e1fad2e1bdc1c7b3dadee79e462ba
bswiley <bswiley@gmail.com>
2023-08-10 19:31:42 -0500

    added to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,28 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />,
+      },
+      {
+        path: '/saved',
+        element: <SavedBooks />,
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
