bbabdf6e86de3ce68c8cf97a350ace3983938100
thoak90codes <adorahtho@gmail.com>
2023-08-11 21:31:08 -0500

    added arguements to User query. the userId cannot be null and when fetching the userId when looking for a user.

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,10 +1,10 @@
 import {gql} from '@apollo/client';
 
 export const GET_USER = gql`
-query User {
-    user {
+query User($userId: ID!) {
+    user(id: $userId) {
+        _id
         username
         email
-        password
     }
 }`
