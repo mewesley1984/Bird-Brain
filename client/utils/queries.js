ff58344657fc789ac0160d9325601a630b7ccc13
thoak90codes <adorahtho@gmail.com>
2023-08-11 21:53:45 -0500

    added GetBirds and GetComments queries to queries.js file

--- client/utils/queries.js
+++ client/utils/queries.js
@@ -1,10 +1,36 @@
 import {gql} from '@apollo/client';
 
 export const GET_USER = gql`
-query User($userId: ID!) {
+query GetUser($userId: ID!) {
     user(id: $userId) {
         _id
         username
         email
+    }
+}`
+
+export const GET_BIRDS = gql`
+query GetBirds {
+    birds {
+        _id
+        description
+        birdId
+    }
+}`
+
+export const GET_COMMENTS = gql`
+query GetComments {
+    comments {
+        _id
+        text
+        datePosted
+        bird {
+            _id
+            description
+        }
+        user {
+            _id
+            username
+        }
     }
 }`
