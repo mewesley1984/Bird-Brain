0e7d613e72d30737c4b538b12731c10b53e2452f
bswiley <bswiley@gmail.com>
2023-08-14 17:30:58 -0500

    corrected the savedBirds reference in the User.js model

--- server/models/User.js
+++ server/models/User.js
@@ -1,35 +1,37 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const birdSchema = require('./Bird');
-
 const userSchema = new Schema(
-    {
-      username: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must use a valid email address'],
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-
-      savedBirds: [birdSchema],
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        toJSON: {
-          virtuals: true,
-        },
-      }
-    );
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
 
+    savedBirds: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Bird', // Reference to the Bird model
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 // hash user password
 userSchema.pre('save', async function (next) {
@@ -41,11 +43,11 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-
 // custom method to compare and validate password for logging in
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
-    const User = model('User', userSchema);
+
+const User = model('User', userSchema);
 
 module.exports = User;
