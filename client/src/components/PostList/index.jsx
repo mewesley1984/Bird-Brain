3d7a03dcf8e072e979a70ebf3efe9200069c1cee
thoak90codes <adorahtho@gmail.com>
2023-08-17 00:32:29 -0500

    committing up to date code

--- client/src/components/PostList/index.jsx
+++ client/src/components/PostList/index.jsx
@@ -7,6 +7,7 @@ import { ADD_COMMENT } from "../../utils/mutations";
 import { useState } from "react";
 import { Link } from 'react-router-dom';
 import Auth from "../../utils/auth";
+import CommentEntry from "../CommentEntry";
 
 const PostList = () => {
   const { loading, data } = useQuery(GET_BIRD_POSTS);
@@ -48,44 +49,29 @@ const PostList = () => {
       {data?.birds?.map(
         (post, i) =>
           i < 6 && (
-            <Link to={`/birds/${bird._id}`} key={i}>
-              <Card border="dark">
-                {post.birdImage ? (
-                  <Card.Img
-                    style={{
-                      margin: "1rem",
-                      width: "5rem",
-                      borderRadius: "5px",
-                    }}
-                    src={post.birdImage}
-                    alt={`This is a ${post.birdName}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{post.birdName}</Card.Title>
-                  <Card.Text>
-                    Author: {post.birdAuthor} <br />
-                    Date: {post.datePosted} <br />
-                    Text: {post.postText}
-                  </Card.Text>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlTextarea1"
-                    onSubmit={handleFormSubmit}
-                  >
-                    <Form.Control
-                      name="commentText"
-                      value={commentText}
-                      onChange={handleChange}
-                      as="textarea"
-                      rows={3}
-                    />
-                  </Form.Group>
-                  <Button variant="primary">Add Comment</Button>{" "}
-                </Card.Body>
-              </Card>
-            </Link>
+            <Card border="dark" class='postResults' key={i}>
+              {post.birdImage ? (
+                <Card.Img
+                  style={{
+                    margin: "1rem",
+                    width: "5rem",
+                    borderRadius: "5px",
+                  }}
+                  src={post.birdImage}
+                  alt={`This is a ${post.birdName}`}
+                  variant="top"
+                />
+              ) : null}
+              <Card.Body>
+                <Card.Title>{post.birdName}</Card.Title>
+                <Card.Text>
+                  Author: {post.birdAuthor} <br />
+                  Date: {post.datePosted} <br />
+                  Text: {post.postText}
+                </Card.Text>
+                <CommentEntry birdId={post._id} />
+              </Card.Body>
+            </Card>
           )
       )}
     </div>
