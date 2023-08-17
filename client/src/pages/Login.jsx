60a864662830d85ce2dbcb1e5cd392f1bc8cd8c6
Chris Messa <blindsun95@gmail.com>
2023-08-17 17:56:23 -0500

    updated login and sign up form styles

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Login = (props) => {
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
         <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <h4 className="card-header p-2" style={{ backgroundColor: '#333000', color: 'wheat' }}>Login</h4>
           <div className="card-body">
             {data ? (
               <p>
@@ -70,8 +70,8 @@ const Login = (props) => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
+                  className="btn btn-block"
+                  style={{ cursor: 'pointer', backgroundColor: '#333000', color: 'wheat' }}
                   type="submit"
                 >
                   Submit
